e8cd7df64b143e61330860eb81ff4471c5c474b5
hoanganhnguyen <hoanganhnguyen.work@outlook.com>
2024-08-17 16:07:05 +0700

    update package

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    [
-      'react-native-reanimated/plugin',
-      {
-        processNestedWorklets: true,
-      },
-    ],
     ['react-native-worklets-core/plugin'],
-
+    'react-native-reanimated/plugin',
   ],
+  // plugins: [
+  //   [
+  //     'react-native-reanimated/plugin',
+  //     {
+  //       processNestedWorklets: true,
+  //     },
+  //   ],
+  //   ['react-native-worklets-core/plugin'],
+  //
+  // ],
 };
