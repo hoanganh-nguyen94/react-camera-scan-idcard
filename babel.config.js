d8123b8591ae3a6cf0fd8a7329cbf56aabcb5a09
hoanganhnguyen <hoanganhnguyen.work@outlook.com>
2024-08-18 11:22:52 +0700

    todo fix code ios

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'react-native-reanimated/plugin',
-      {
-        processNestedWorklets: true,
-      },
-    ],
-    ['react-native-worklets-core/plugin'],
-
-  ],
+  plugins: [['react-native-worklets-core/plugin']],
 };
