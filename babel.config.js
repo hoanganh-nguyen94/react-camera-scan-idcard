a4c60889fc6b054802eb2ec57ae7d4d1ad5b2d05
Hoang Anh Nguyen <anh.nh@hsc.com.vn>
2024-08-21 17:15:31 +0700

    downgrade version

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'react-native-reanimated/plugin',
-      {
-        processNestedWorklets: true,
-      },
-    ],
-    ['react-native-worklets-core/plugin'],
-
-  ],
 };
