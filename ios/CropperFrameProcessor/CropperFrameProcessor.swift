1eaeb619b1152b964d0e81310350d3c2b4e655c6
hoanganhnguyen <hoanganhnguyen.work@outlook.com>
2024-08-21 12:58:27 +0700

    todo clone cropper framer

--- ios/CropperFrameProcessor/CropperFrameProcessor.swift
+++ ios/CropperFrameProcessor/CropperFrameProcessor.swift
@@ -1,34 +1,68 @@
 import VisionCamera
 
-@objc(CropperFrameProcessorPlugin)
+@objc(
+  CropperFrameProcessorPlugin
+)
 public class CropperFrameProcessorPlugin: FrameProcessorPlugin {
-  public override init(proxy: VisionCameraProxyHolder, options: [AnyHashable: Any]! = [:]) {
-    super.init(proxy: proxy, options: options)
+  public override init(
+    proxy: VisionCameraProxyHolder,
+    options: [AnyHashable: Any]! = [:]
+  ) {
+    super.init(
+      proxy: proxy,
+      options: options
+    )
   }
   
-  public override func callback(_ frame: Frame, withArguments arguments: [AnyHashable: Any]?) -> Any? {
+  public override func callback(
+    _ frame: Frame,
+    withArguments arguments: [AnyHashable: Any]?
+  ) -> Any? {
     let buffer = frame.buffer
     var cropResult: [String:Any] = [:]
-    guard let imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer) else {
-      print("Failed to get CVPixelBuffer!")
+    guard let imageBuffer = CMSampleBufferGetImageBuffer(
+      frame.buffer
+    ) else {
+      print(
+        "Failed to get CVPixelBuffer!"
+      )
       return cropResult
     }
-    let ciImage = CIImage(cvPixelBuffer: imageBuffer)
+    let ciImage = CIImage(
+      cvPixelBuffer: imageBuffer
+    )
     
-    guard let cgImage = CIContext().createCGImage(ciImage, from: ciImage.extent) else {
-      print("Failed to create CGImage!")
+    guard let cgImage = CIContext().createCGImage(
+      ciImage,
+      from: ciImage.extent
+    ) else {
+      print(
+        "Failed to create CGImage!"
+      )
       return cropResult
     }
     
     let image:UIImage;
     let cropRegion = arguments?["cropRegion"] as? [String: Int]
     if cropRegion != nil {
-      let imgWidth = Double(cgImage.width)
-      let imgHeight = Double(cgImage.height)
-      let left:Double = Double(cropRegion?["left"] ?? 0) / 100.0 * imgWidth
-      let top:Double = Double(cropRegion?["top"] ?? 0) / 100.0 * imgHeight
-      let width:Double = Double(cropRegion?["width"] ?? 100) / 100.0 * imgWidth
-      let height:Double = Double(cropRegion?["height"] ?? 100) / 100.0 * imgHeight
+      let imgWidth = Double(
+        cgImage.width
+      )
+      let imgHeight = Double(
+        cgImage.height
+      )
+      let left:Double = Double(
+        cropRegion?["left"] ?? 0
+      ) / 100.0 * imgWidth
+      let top:Double = Double(
+        cropRegion?["top"] ?? 0
+      ) / 100.0 * imgHeight
+      let width:Double = Double(
+        cropRegion?["width"] ?? 100
+      ) / 100.0 * imgWidth
+      let height:Double = Double(
+        cropRegion?["height"] ?? 100
+      ) / 100.0 * imgHeight
       
       // The cropRect is the rect of the image to keep,
       // in this case centered
@@ -42,29 +76,51 @@ public class CropperFrameProcessorPlugin: FrameProcessorPlugin {
       let cropped = cgImage.cropping(
         to: cropRect
       )!
-      image = UIImage(cgImage: cropped)
-      print("use cropped image")
+      image = UIImage(
+        cgImage: cropped
+      )
+      print(
+        "use cropped image"
+      )
     }else{
-      image = UIImage(cgImage: cgImage)
+      image = UIImage(
+        cgImage: cgImage
+      )
     }
     let includeImageBase64 = arguments!["includeImageBase64"] as? Bool ?? false
     if includeImageBase64 == true {
-      cropResult["base64"] = getBase64FromImage(image)
+      cropResult["base64"] = getBase64FromImage(
+        image
+      )
     }
     
     return cropResult
   }
   
-  func saveImage(_ image:UIImage) -> String {
+  func saveImage(
+    _ image:UIImage
+  ) -> String {
     let url = FileManager.default.temporaryDirectory
-      .appendingPathComponent(UUID().uuidString)
-      .appendingPathExtension("jpeg")
-    try? image.jpegData(compressionQuality: 1.0)?.write(to: url)
+      .appendingPathComponent(
+        UUID().uuidString
+      )
+      .appendingPathExtension(
+        "jpeg"
+      )
+    try? image.jpegData(
+      compressionQuality: 1.0
+    )?.write(
+      to: url
+    )
     return url.path
   }
   
-  func getBase64FromImage(_ image:UIImage) -> String {
-    let dataTmp = image.jpegData(compressionQuality: 100)
+  func getBase64FromImage(
+    _ image:UIImage
+  ) -> String {
+    let dataTmp = image.jpegData(
+      compressionQuality: 100
+    )
     if let data = dataTmp {
       return data.base64EncodedString()
     }
